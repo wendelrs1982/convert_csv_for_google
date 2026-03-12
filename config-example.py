31ccfd98c7a7d628457659513716fd3621999a1f
wendelrs1982 <wendel.rs@gmail.com>
2026-03-12 16:43:50 -0300

    Ajustando os paramêtros do config-example

--- config-example.py
+++ config-example.py
@@ -1,15 +1,14 @@
 # Configurações Gerais
 ANO_AGENDA="0000"
 LOCAL_EVENTO = "A definir"
+PASTA_OUT = "out"
+PASTA_IN = "in"
 
 # Configurações do XLSX
-PASTA_IN_XLSX = "INFORME_O_NOME_DA_PASTA"
 NOME_ARQUIVO_XLSX = "INFORME_O_NOME_DO_ARQUIVO.xlsx"
 
-# Informações do novo arquivo xlsx, contendo as linhas que não foram importadas para o CSV
-PASTA_XLSX_NAO_PROCESSADOS = "INFORME_O_NOME_DA_PASTA"
-ARQUIVO_XLSX_NAO_PROCESSADOS = "INFORME_O_NOME_DO_ARQUIVO.xlsx"
-
 # Configurações do CSV
-PASTA_OUT_CSV = "INFORME_O_NOME_DA_PASTA"
 NOME_ARQUIVO_CSV = "INFORME_O_NOME_DO_ARQUIVO.csv"
+
+# Informações do novo arquivo xlsx, contendo as linhas que não foram importadas para o CSV
+ARQUIVO_XLSX_NAO_PROCESSADOS = "INFORME_O_NOME_DO_ARQUIVO.xlsx"
